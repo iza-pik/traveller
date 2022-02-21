8d09ca3e481e3ecf6deda84f38e891776abf3247
Izabela Pikula <izapik@Izabelas-MacBook-Pro.local>
2022-02-21 02:04:58 +0000

    Initial commit - fix test

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { screen } from '@testing-library/react'
 import { render } from './test-utils'
 import { App } from './App'
+import { MockedProvider } from '@apollo/client/testing'
 
 describe('<App /> component', () => {
   it('renders the Header content', () => {
     render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <MockedProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </MockedProvider>
     )
     const HeadingComponent = screen.getByText(/^Smart traveller$/i)
     expect(HeadingComponent).toBeInTheDocument()
